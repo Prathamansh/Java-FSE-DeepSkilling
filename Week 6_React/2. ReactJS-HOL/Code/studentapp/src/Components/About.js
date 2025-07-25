14c6826c872f01569abf0efd816de5c401944b40
Prathamansh Jasrotia <164734941+Prathamansh@users.noreply.github.com>
2025-07-25 15:40:05 +0530

    Add files via upload

---
+++ Week 6_React/2. ReactJS-HOL/Code/studentapp/src/Components/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function About() {
+  return (
+    <div>
+      <h2>Welcome to the About page of Student Management Portal</h2>
+    </div>
+  );
+}
+
+export default About;
